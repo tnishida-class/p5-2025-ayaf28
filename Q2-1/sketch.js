591434a76014435b1f80a94841c6f7292dd055e4
ayaf28 <cfuyong7@gmail.com>
2025-10-21 12:08:01 +0900

    section2

--- Q2-1/sketch.js
+++ Q2-1/sketch.js
@@ -1,8 +1,17 @@
 // 小手調べ
 function setup(){
   createCanvas(100, 100);
-  noFill();
   for(let i = 0; i < 10; i++){
+    let size = (i + 1) * 10;
+  noFill();
     // BLANK[1] 条件分岐を使って、前半は赤、後半は青で円を描こう
+
+    if(i < 5){
+      stroke(0, 0, 255); // 青
+    } else {
+      stroke(255, 0, 0); // 赤
+    }
+    ellipse(50, 50, size, size);
   }
 }
+
--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -5,7 +5,19 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if((i + j) % 2 == 1){
       // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+      fill(150);
+      rect(size*i,size*j,size,size);
+      }
+    if(j < 3 && (i+j) % 2 == 1){
+      fill(255,0,0);
+      ellipse(size*0.5 + size*i,size*0.5 + size*j,size*0.9,size*0.9);
+      }
+    if(j > 4 && (i+j) % 2 == 1){
+      fill(0);
+      ellipse(size*0.5 + size*i,size*0.5 + size*j,size*0.9,size*0.9);
     }
-  }
+   }
+  }  
 }
